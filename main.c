0422db9cb5d01a8bd043c4437578395fa61319b2
danwritecode <dnelson4993@outlook.com>
2024-05-31 22:56:03 -0400

    rendering pixels as rectangles

--- main.c
+++ main.c
@@ -2,22 +2,33 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct {
     int x;
     int y;
 } Point;
 
+typedef struct {
+    int posX;
+    int posY;
+    int width;
+    int height;
+    Color color;
+} RPixel;
+
 void RenderStandard(Image *image, int offset);
 void RenderOldTv(Image *image, int offset);
 void RenderBooting(Image *image, int offset, int render_rate);
+void RenderBlocks(Image *image, int offset, int pixel_size, int scale_factor);
 void ImageQuantizeEuclidean(Image *image);
 void ImageQuantizeManhattan(Image *image);
 void ImagePixelate(Image *image, int pixel_size);
-void ImagePixelateRetainPixelCt(Image *image, int pixel_size);
+void ImagePixelateDegrade(Image *image, int pixel_size);
 Point GetPointFromIndex(int pixel, int width);
 int GetIndexFromPoint(Point point, int width);
 Image ImageResizeNearestNeighbor(Image image, int newWidth, int newHeight);
+void delay(int seconds);
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 800;
@@ -41,16 +52,15 @@ int main() {
     ImageResize(&image, 400, 600);
 
     ImageQuantizeManhattan(&image);
-    ImagePixelate(&image, 49);
-
-    Image new_image = ImageResizeNearestNeighbor(image, 400, 600);
+    ImagePixelate(&image, 16);
 
     SetTargetFPS(60);
     int offset = 100;
 
     // RenderBooting(&new_image, offset, 1);
     // RenderOldTv(&image, offset);
-    RenderStandard(&new_image, offset);
+    // RenderStandard(&image, offset);
+    RenderBlocks(&image, offset, 16, 5);
 
     CloseWindow();
     return 0;
@@ -69,6 +79,54 @@ void RenderStandard(Image *image, int offset) {
     }
 
     UnloadTexture(texture);
+}
+
+void RenderBlocks(Image *image, int offset, int pixel_size, int scale_factor) {
+    Color *pixels = LoadImageColors(*image);
+    int block_side_len = sqrt(pixel_size);
+    int height = image->height / block_side_len;
+    int width = image->width / block_side_len;
+    RPixel *rpixels = malloc(sizeof(RPixel) * (height * width));
+    int rpix_size = 0;
+
+    for (int pi = 0; pi < (height * width); pi++) {
+	Point block_point = GetPointFromIndex(pi, width);
+
+	int og_x = block_point.x * block_side_len;
+	int og_y = block_point.y * block_side_len;
+	Point og_point = {og_x, og_y};
+	int og_index = GetIndexFromPoint(og_point, width * block_side_len);
+	Color og_pixel = pixels[og_index];
+
+	if (og_pixel.a != 0) {
+	    rpixels[rpix_size].posX = block_point.x * scale_factor;
+	    rpixels[rpix_size].posY = block_point.y * scale_factor;
+	    rpixels[rpix_size].width = block_side_len * scale_factor;
+	    rpixels[rpix_size].height = block_side_len * scale_factor;
+	    rpixels[rpix_size].color = og_pixel;
+
+	    rpix_size++;
+	}
+    }
+
+    int index = 0;
+    while (!WindowShouldClose()) {
+	BeginDrawing();
+	// ClearBackground(BLACK);
+	
+	// if (index < rpix_size) {
+	//     DrawRectangle(rpixels[index].posX, rpixels[index].posY, rpixels[index].width, rpixels[index].height, rpixels[index].color);
+	// }
+
+	for (int i = 0; i < rpix_size; i++) {
+	    DrawRectangle(rpixels[i].posX, rpixels[i].posY, rpixels[i].width, rpixels[i].height, rpixels[i].color);
+	}
+
+	index++;
+	EndDrawing();    
+    }
+    UnloadImage(*image);
+    free(rpixels);
 }
 
 void RenderOldTv(Image *image, int offset) {
@@ -223,7 +281,8 @@ void ImageQuantizeManhattan(Image *image) {
 }
 
 /// Pixel Size must be square rootable with no remainder
-void ImagePixelate(Image *image, int pixel_size) {
+/// Pixel count actually reduces here and needs to be scaled back up if wanted
+void ImagePixelateDegrade(Image *image, int pixel_size) {
     ImageAlphaPremultiply(image);
     Color *pixels = LoadImageColors(*image);
 
@@ -279,31 +338,31 @@ void ImagePixelate(Image *image, int pixel_size) {
 }
 
 /// Pixel Size must be square rootable with no remainder
-void ImagePixelateRetainPixelCt(Image *image, int pixel_size) {
+void ImagePixelate(Image *image, int pixel_size) {
     if ((image->data == NULL) || (image->width == 0) || (image->height == 0)) return;
 
     ImageAlphaPremultiply(image);
     Color *pixels = LoadImageColors(*image);
-    int pixel_lh = sqrt(pixel_size);
-    int height = image->height / pixel_lh;
-    int width = image->width / pixel_lh;
+    int block_side_len = sqrt(pixel_size);
+    int height = image->height / block_side_len;
+    int width = image->width / block_side_len;
 
     for (int b = 0; b < (width * height); b++) {
 	Point point = GetPointFromIndex(b, width);
 	
 	// find original pixel index
-	int og_x = point.x * pixel_lh;
-	int og_y = point.y * pixel_lh;
+	int og_x = point.x * block_side_len;
+	int og_y = point.y * block_side_len;
 	Color block_pixels[pixel_size];
 	int pixel_idxs[pixel_size];
 
-	for (int x = 0; x < pixel_lh; x++) {
-	    for (int y = 0; y < pixel_lh; y++) {
+	for (int x = 0; x < block_side_len; x++) {
+	    for (int y = 0; y < block_side_len; y++) {
 		Point point = {x + og_x, y + og_y};
 		int pixel_idx = GetIndexFromPoint(point, image->width);
 
 		Point block_point = {x, y};
-		int block_idx = GetIndexFromPoint(block_point, pixel_lh);
+		int block_idx = GetIndexFromPoint(block_point, block_side_len);
 
 		block_pixels[block_idx] = pixels[pixel_idx];
 		pixel_idxs[block_idx] = pixel_idx;
@@ -373,3 +432,10 @@ Image ImageResizeNearestNeighbor(Image image, int newWidth, int newHeight) {
 
     return new_image;
 }
+
+void delay(int seconds) {
+    int m_secs = 1 * seconds;
+    clock_t start_time = clock();
+
+    while (clock() < start_time + m_secs) {};
+} 
